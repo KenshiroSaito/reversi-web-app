1eec403d5dff189ff7e576d459951142c6160f90
NoirMakkuro <eigo.kenshiro.cap@gmail.com>
2026-06-30 20:10:05 -0700

    Remove legacy MySQL data access layer

--- src/dataaccess/moveRecord.ts
+++
@@ -1,9 +0,0 @@
-export class MoveRecord {
-  constructor(
-    private _id: number,
-    private _turnId: number,
-    private _disc: number,
-    private _x: number,
-    private _y: number,
-  ) {}
-}
