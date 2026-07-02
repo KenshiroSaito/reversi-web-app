8ccb3668fab746e54902e595e4d5efc8999472df
NoirMakkuro <eigo.kenshiro.cap@gmail.com>
2026-07-01 20:43:52 -0700

    Refactor TurnService and related models: enhance type safety for disc and point, add validation for disc and point values, and update turn registration logic

--- src/application/service/turnService.ts
+++ src/application/service/turnService.ts
@@ -1,6 +1,6 @@
 import { connectMySQL } from "../../infrastructure/connection";
 import { GameRepository } from "../../domain/model/game/gameRepository";
-import { toDisc } from "../../domain/model/turn/disc";
+import { Disc, toDisc } from "../../domain/model/turn/disc";
 import { Point } from "../../domain/model/turn/point";
 import { TurnRepository } from "../../domain/model/turn/turnRepository";
 import { ApplicationError } from "../error/applicationError";
@@ -70,7 +70,7 @@ export class TurnService {
     }
   }
 
-  async registerTurn(turnCount: number, disc: number, x: number, y: number) {
+  async registerTurn(turnCount: number, disc: Disc, point: Point) {
     const conn = await connectMySQL();
 
     try {
@@ -95,7 +95,7 @@ export class TurnService {
       );
 
       // Place a stone
-      const newTurn = previousTurn.placeNext(toDisc(disc), new Point(x, y));
+      const newTurn = previousTurn.placeNext(disc, point);
 
       // Save the turn
       await turnRepository.save(conn, newTurn);
